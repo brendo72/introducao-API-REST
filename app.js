df8158a0586be4140dbc9e05d57ee9e75b46a0ee
brendo 12 <brendobraga72@gmail.com>
2025-04-22 13:34:16 -0300

    add:atualizaçao 1.0

--- app.js
+++ app.js
@@ -1,16 +1,67 @@
-import express from "express"
-const app = express()
-const port = 3000
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.use(express.json());
 
 const usuarios = [
-  {id:1,nome:'joao', email:'joao@hotmail.com'},
-  {id:2,nome:'maria', email:'maria@hotmail.com'}
-]
+  { id: 1, nome: "João", email: "joao@email.com" },
+  { id: 2, nome: "Ana", email: "ana@email.com" },
+];
 
-app.get('/usuario', (req, res) => {
-  res.send(usuarios)
-})
+// Rota inicio
+app.get("/", (req, res) => {
+  res.send("Bem vindo a minha API");
+});
+
+// Rota cria usuario
+app.post("/criarUsuario", (req, res) => {
+  const { nome, email } = req.body;
+
+  // Adicionar o usuario no banco de dados fake(lista)
+  const novoUsuario = {
+    id: usuarios[usuarios.length - 1].id + 1,
+    nome: nome,
+    email: email,
+  };
+  usuarios.push(novoUsuario);
+
+  res.send(usuarios);
+});
+
+app.put("/usuario/:id", (req, res) => {
+  const { id } = req.params;
+  const { novoNome, novoEmail } = req.body;
+
+  const indice = usuarios.findIndex((usuario) => {
+    return usuario.id == id;
+  });
+  usuarios[indice].nome = novoNome;
+  usuarios[indice].email = novoEmail;
+
+  res.send(usuarios);
+});
+
+// Deletar usuario
+app.delete("/usuario/:id", (req, res) => {
+  const { id } = req.params;
+
+  const index = usuarios.findIndex((usuario) => {
+    return usuario.id === id;
+  });
+
+  if (index === -1) {
+    res.send("Usuario nao encontrado!");
+  } else {
+    usuarios.splice(index, 1);
+    res.send(usuarios);
+  }
+
+  usuarios.splice(index, 1);
+
+  res.send(usuarios);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
